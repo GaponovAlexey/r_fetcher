b76db015dcb00deb115c626eca656b01ea82d45a
Kekelberi <alexei.gaponov@gmail.com>
2023-07-30 12:43:55 -0500

    save

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,9 @@
-use std::cell::RefCell;
-use std::rc::Rc;
+mod lib;
 
-#[derive(Debug)]
-enum BitBlock {
-    Block(i32, RefCell<Rc<BitBlock>>),
-    Genesis,
-}
-
-use BitBlock::{ Block, Genesis };
-
-impl BitBlock {
-    fn get_prev_block(&self) -> Option<&RefCell<Rc<BitBlock>>> {
-        match self {
-            Self::Block(_, i) => Some(i),
-            Genesis => None,
-        }
-    }
-}
+use rand::Rng;
 
 fn main() {
-    let bl1 = Rc::new(Block(1, RefCell::new(Rc::new(BitBlock::Genesis))));
-    println!("{:?}", Rc::strong_count(&bl1))
+    lib::development::dev::add_task();
+    println!("r : {:?}", rand::thread_rng().gen_range(1..100));
+    println!("HI")
 }
