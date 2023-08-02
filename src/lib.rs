cd6b72529c65e00aebb8a3150f59d92d6f75c411
Kekelberi <alexei.gaponov@gmail.com>
2023-08-01 19:25:17 -0500

    test

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,6 @@
 pub mod dev;
 pub mod testsend;
+
+pub fn add_one(x: i32) -> i32 {
+    x + 2
+}
--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,7 @@
-use std::sync::{ Arc, RwLock };
-use std::thread;
-
+use r_fetcher::add_one;
 fn main() {
-    let data = Arc::new(RwLock::new(0));
-
-    let mut handles = vec![];
-
-    // Спавним 10 потоков, которые увеличивают значение
-    for _ in 0..10 {
-        let data = Arc::clone(&data);
-
-        let handle = thread::spawn(move || {
-            let mut num = data.write().unwrap();
-            *num += 10;
-        });
-
-        handles.push(handle);
-    }
-
-    // Спавним 10 потоков, которые читают значение
-    for _ in 0..10 {
-        let data = Arc::clone(&data);
-
-        let handle = thread::spawn(move || {
-            let num = data.read().unwrap();
-
-            println!("r: {}", *num);
-        });
-
-        handles.push(handle);
-    }
-
-    // Дожидаемся завершения всех потоков
-    for handle in handles {
-        println!("{:?}", handle);
-        handle.join().unwrap();
-    }
+    let x = add_one(5);
+    println!("{:?}", x);
+    let answer = add_one(5);
+    assert_eq!(7, answer)
 }
