30a87abaf0d205201fbaee9445d79a2b36f717c3
Kekelberi <alexei.gaponov@gmail.com>
2023-08-03 18:03:36 -0500

    save

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,5 @@
-struct MyIter<'a, T> {
-    slice: &'a [T],
-}
-
-impl<'a, T> Iterator for MyIter<'a, T> {
-    type Item = &'a T;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let elm = self.slice.get(0);
-        if self.slice.len() > 0 {
-            self.slice = &self.slice[1..];
-        }
-        elm
-    }
-}
-
+use std::io::stdin;
+use rand::prelude::*;
 fn main() {
-    let col = vec![1, 2, 3, 4];
-    let iter = MyIter { slice: &col[..] };
-    for (i, e) in iter.enumerate() {
-        print!("E = {:?} :", e);
-        println!("ind :{:?}", i);
-        assert_eq!(*e, col[i]);
-    }
+    println!("Ukraine ⛳")
 }
