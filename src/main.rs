e32955143b2e666a934f9a18e85ad80f0a3a1512
Kekelberi <alexei.gaponov@gmail.com>
2023-08-04 00:09:58 -0500

    serde

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::{ fs::File, io::Read };
+
+use serde_json::{ self, Value };
+
+use serde::{ Serialize, Deserialize };
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Dev {
+    name: String,
+    age: i32,
+    address: Address,
+    phones: Vec<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Address {
+    street: String,
+    city: String,
+}
 fn main() {
-    
+    let mut file = File::open("src/data.json").expect("err open file");
+    let mut contents = "".to_string();
+    file.read_to_string(&mut contents).expect("err read");
+    let deserialized: Dev = serde_json::from_str(&contents).expect("err serde");
+    println!("{:?}", deserialized);
 }
